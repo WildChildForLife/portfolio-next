baea75245ed0d862782d50e5dd67b102bf9d1f09
Youssef El Gharbaoui <youssef.elgharbaoui@gmail.com>
2023-09-07 13:56:29 +0200

    Add Nb Experience to Experience Section

--- src/app/components/SkillsExperiences/SkillsExperiences.tsx
+++ src/app/components/SkillsExperiences/SkillsExperiences.tsx
@@ -5,9 +5,10 @@ const SkillsExperiences: React.FC<SkillsExperiencePropsType> = ({ id, yearsOfExp
     return (
         <section id={id} className="skills-experiences-section container mx-auto">
             <h2>Tech & Experience</h2>
-            <h3>{yearsOfExperience} of experience</h3>
-            <p>With over 14 years of experience in the tech industry. My journey as an Architect, started when I was a curious little coder. Passion and ambition have always fueled my architectural adventures, making every project a playground of possibilities. </p>
-            <p>From shaping systems to building grand designs, I weave my way through the code, crafting masterpieces that stand the test of time. As an architect through coding, I unlock the realms of creativity, constructing digital wonders that leave a mark on the world of tech.</p>
+            <h3>{yearsOfExperience} years of experience</h3>
+            <p>My journey as an Architect, started when I was a curious little coder. Passion and ambition have always fueled my adventures, making every project a playground of possibilities. </p>
+            <p>From shaping systems to building grand designs, I weave my way through the code, crafting masterpieces that stand the test of time.</p>
+            <p>I unlock the realms of creativity, constructing digital wonders that leave a mark on the world of tech.</p>
             <blockquote className="blockquote">&#32;Some of the greatest accomplishments in life come from tackling challenges that initially seemed beyond our capabilities.</blockquote>
             <div className="experiences">
                 <Experiences></Experiences> 
